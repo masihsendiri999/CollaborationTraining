d67ffc1ae9220f18276b77211aec3da099f31fa6
chessaAditirta <chessatirta@gmail.com>
2019-01-29 01:09:37 +0700

    Mengedit file Index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo WP(155610024)</h3>"
+echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
 
 ?>
